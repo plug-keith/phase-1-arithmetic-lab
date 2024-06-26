707f4b3f376b18fd6eb9c193ad1ff6291e7615f6
plug_keith <keith.mwai@student.moringaschool.com>
2024-06-26 17:58:39 +0300

    assigment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let random = Math.floor(+1)
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2
+
+
+
+let num3 = 1
+let num4 = 0
+
+let mod = (num4*num3) + 4
+
+let max = 20
